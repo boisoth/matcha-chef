c2370a4372e5078b60d55631487386db1104c592
Boi Soth <boi@cuey.io>
2019-11-06 22:29:12 -0800

    Creating a new user and checking if user exist in collection. Setting up the avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const gravatar = require("gravatar");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Register user
@@ -20,7 +23,7 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     // Check for errors from request body
     const errors = validationResult(req);
 
@@ -28,8 +31,44 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("User route");
+    // Destructure body
+    const { name, email, password } = req.body;
+
+    // Check if the user exists
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exist" }] });
+      }
+
+      // Setting gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mn" // for default user icon if no gravatar
+      });
+
+      // Create a new user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      res.send("User route");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
 module.exports = router;
+
+// Get users gravatar
+
+// Encrypt  password
+
+// Return JWT
