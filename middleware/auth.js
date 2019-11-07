32ec32b33123203221e3d40a211ca50a5d0b1b22
Boi Soth <boi@cuey.io>
2019-11-06 23:56:13 -0800

    Adding middleware to verify jwt on a protected route

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req, res, next) {
+  // Get token from the header
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    console.log(decoded);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ msg: "Token is not valid!!!!" });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 // @route   GET api/auth
 // @desc    Auth route
-// @access  Public
-router.get("/", (req, res) => res.send("Auth route"));
+// @access  Private
+router.get("/", auth, (req, res) => res.send("Auth route"));
 
 module.exports = router;
