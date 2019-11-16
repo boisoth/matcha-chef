1dc815cc5d67d7678ff2188fcbcb4acf53f86818
Boi Soth <boi@cuey.io>
2019-11-16 00:13:16 -0800

    adding some static views with ejs

--- server.js
+++ server.js
@@ -7,17 +7,30 @@ const PORT = process.env.PORT || 5000;
 // Connect to Database
 connectDB();
 
+// EJS Config
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Routes
+// API Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+// Views
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
