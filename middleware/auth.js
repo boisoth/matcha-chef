e6d12e98b4f0368ff11bf4de40e274d1399a1779
Boi Soth <naneang415@gmail.com>
2019-11-29 20:32:15 -0800

    documenting for jwt verify

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,14 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
+// Middleware to pass into protected routes before callback req/res
+// After registration of users, JWT will sign a new token to that user
+// We then pass that token in from the request header to authenticate
+// When we recieve the token in the header, we call the jwt.verify() method
+// with the token(user id etc) and the secret.
+// We recieve a callback with the verified users, store it as the req.user and pass it
+// to any other next() middleware within the chain
+
 module.exports = function(req, res, next) {
   // Get token from the header
   const token = req.header("x-auth-token");
@@ -11,6 +19,7 @@ module.exports = function(req, res, next) {
 
   // Verify token
   try {
+    //
     const decoded = jwt.verify(token, config.get("jwtSecret"));
     console.log(decoded);
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,8 @@ const auth = require("../../middleware/auth");
 // @route   GET api/auth
 // @desc    Auth route
 // @access  Private
+
+// auth middleware docodes and verify the jwt
 router.get("/", auth, (req, res) => {
   res.send("Auth route");
 });
