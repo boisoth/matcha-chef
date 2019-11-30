f71ae972c454175072f9aada81c352ccbce27e50
Boi Soth <naneang415@gmail.com>
2019-11-30 02:06:02 -0800

    Creating user profile route/me

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route   GET api/profile
-// @desc    Profile route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }); // User ID is passed from auth middleware which exposed the ID then used in Profile Schema user field type -- mongoose.Schema.Types.ObjectId
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
