aaa47293f61167798e3a150fdfabc0e2e3a24515
Boi Soth <naneang415@gmail.com>
2019-12-12 13:35:37 -0800

    separating views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,17 +24,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-// Views
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+// View Routes
+app.use(htmlRoutes);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
