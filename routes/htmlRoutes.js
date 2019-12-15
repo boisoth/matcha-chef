b8469ab53c220ee0ff848aa5267cbaac6ed8fa94
Boi Soth <naneang415@gmail.com>
2019-12-15 11:56:02 -0800

    passing title from server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 // Views
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 router.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 router.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", { title: "Contact" });
 });
 
 module.exports = router;
