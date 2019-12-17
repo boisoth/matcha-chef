ac0a54f957d80aa54b5ba158f6a30e18b5402484
Boi Soth <naneang415@gmail.com>
2019-12-17 15:56:57 -0800

    adding posts api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -25,6 +25,25 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      // User model for new post
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
