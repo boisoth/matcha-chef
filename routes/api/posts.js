e4c67f707514e0cc48e38a359118701653611cfe
Boi Soth <naneang415@gmail.com>
2019-12-17 00:12:14 -0800

    dependencies for posts api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route   GET api/posts
-// @desc    Posts route
-// @access  Public
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
 router.get("/", (req, res) => res.send("Posts route"));
 
 module.exports = router;
