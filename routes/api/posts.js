8c2d1051c50abfeb0ff526a48cb7e8d0ef30a74a
Boi Soth <naneang415@gmail.com>
2020-01-02 15:59:20 -0800

    getting posts sorted

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -53,7 +53,9 @@ router.post(
 
 router.get("/", auth, async (req, res) => {
   try {
-    const posts = await Post.find();
+    const posts = await Post.find().sort({date: -1});
+    res.json(posts);
+    
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
