709f9eec06ebfd90ed5851f528a9627bebf9f46c
Boi Soth <naneang415@gmail.com>
2020-01-02 16:42:50 -0800

    get a post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -62,4 +62,28 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if(!post){
+      return res.status(404).json({ msg: "Post not found"})
+    }
+    res.json(posts);
+
+  } catch (err) {
+    console.error(err.message);
+
+    if(err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found"})
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
