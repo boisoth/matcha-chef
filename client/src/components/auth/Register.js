678df959f063bf23ed786ecf9497defc8f21e48c
Boi Soth <naneang415@gmail.com>
2020-01-06 00:40:34 -0800

    onChange handler

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,13 +11,14 @@ const Register = () => {
     });
 
     const { name, email, passowrd, password2 } = formData;
-    
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+
     return <Fragment>
         <h1 className="large text-primary">Sign Up</h1>
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
       <form className="form" action="create-profile.html">
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email" />
