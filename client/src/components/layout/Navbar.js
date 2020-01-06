2dc058b96066f42ae11910e24fdd0965ce7e8ab1
Boi Soth <naneang415@gmail.com>
2020-01-05 22:49:55 -0800

    adding static markup for navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,1 +1,17 @@
 /**Navbar */
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+            <h1>
+                <a href="index.html"><i className="fas fa-code"></i> Matcha-Chef</a>
+            </h1>
+            <ul>
+                <li><a href="profiles.html">Profiles</a></li>
+                <li><a href="register.html">Register</a></li>
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
