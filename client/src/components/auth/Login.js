0d56f074fe991e28682b7c3d2523eff01d37af6c
Boi Soth <naneang415@gmail.com>
2020-01-06 00:27:19 -0800

    changing class to className

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
     return <Fragment>
-        <div class="alert alert-danger">
+        <div className="alert alert-danger">
         Invalid credentials
       </div>
-      <h1 class="large text-primary">Sign In</h1>
-      <p class="lead"><i class="fas fa-user"></i> Sign into Your Account</p>
-      <form class="form" action="/dashboard">
-        <div class="form-group">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" action="/dashboard">
+        <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
@@ -17,16 +17,16 @@ const Login = () => {
             required
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
           />
         </div>
-        <input type="submit" class="btn btn-primary" value="Login" />
+        <input type="submit" className="btn btn-primary" value="Login" />
       </form>
-      <p class="my-1">
+      <p className="my-1">
         Don't have an account? <Link to="/register">Sign Up</Link>
       </p>
     </Fragment>
