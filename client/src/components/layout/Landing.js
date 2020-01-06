dd80eae80a2616eba3e6374f9b0c078d9ff4e1f2
Boi Soth <naneang415@gmail.com>
2020-01-05 22:53:08 -0800

    Landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,1 +1,21 @@
-/**Landing */
+import React from 'react'
+
+export const Landing = () => {
+    return (
+        <section className="landing">
+            <div className="dark-overlay">
+                <div className="landing-inner">
+                <h1 className="x-large">Matcha-Chef</h1>
+                <p className="lead">
+                    Create a profile/portfolio, share posts and get help from
+                    other chefs
+                </p>
+                <div className="buttons">
+                    <a href="register.html" className="btn btn-primary">Sign Up</a>
+                    <a href="login.html" className="btn btn-light">Login</a>
+                </div>
+                </div>
+            </div>
+        </section>
+    )
+}
