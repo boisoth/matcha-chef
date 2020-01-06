08e33a60cc779540587eb11fe9813f4861a47a8f
Boi Soth <naneang415@gmail.com>
2020-01-06 01:03:32 -0800

    registration form submit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 const Register = () => {
 
@@ -12,12 +13,32 @@ const Register = () => {
 
     const { name, email, password, password2 } = formData;
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
-    const onSubmit = e => {
+
+    const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
             console.log("Passwords don't match");
         } else {
-            console.log(formData)
+            const newUser = {
+                name,
+                email,
+                password
+            }
+
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': "application/json"
+                    }
+                }
+
+                const body = JSON.stringify(newUser);
+                const res = await axios.post('/api/users', body, config);
+
+                console.log(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
     }
 
