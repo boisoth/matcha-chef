e5b350b8a91f00ba7b9935d1af8acc765aac57c2
Boi Soth <naneang415@gmail.com>
2020-01-05 23:28:58 -0800

    Adding routes and changing href paths

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <a href="index.html"><i className="fas fa-code"></i> Matcha-Chef</a>
+                <a href="/"><i className="fas fa-code"></i> Matcha-Chef</a>
             </h1>
             <ul>
                 <li><a href="profiles.html">Profiles</a></li>
