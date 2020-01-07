40aabcc16ac85b3756adf622cbcd1dccd1339026
Boi Soth <naneang415@gmail.com>
2020-01-07 00:24:26 -0800

    connect module

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
@@ -102,4 +103,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default connect()(Register);
