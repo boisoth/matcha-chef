eb47146fd27ebfcd33cf2307bd5eae360c9fd661
Boi Soth <naneang415@gmail.com>
2020-01-12 11:58:31 -0800

    more commenting

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,6 +2,8 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = [];
 
+// action object with type and payload (msg/alertType/ID)
+
 export default function(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
